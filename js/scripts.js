a4cf06ffcb63ce23161f2d2b0ad98af3643c4d95
anzalmohamed <anzaldidah@gmail.com>
2021-03-01 15:12:36 +0300

    updated files

---
+++ js/scripts.js
@@ -0,0 +1,102 @@
+/*$(document).ready(function(){
+    $("#developmentImage").click(function(){
+      $("#developmentImage").slideDown('1500').hide('1000');
+      $("#development").show('1500');
+    });
+    $("#development").click(function(){
+      $("#development").slideUp('1500');
+      $("#developmentImage").slideDown('1500');
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#design-image").click(function(){
+      $("#design-image").slideDown('1500').hide('1000');
+      $("#design").show('1500');
+    });
+    $("#design").click(function(){
+      $("#design").slideUp('1500');
+      $("#designImage").slideDown('1500');
+    });
+  });
+  $(document).ready(function(){
+    $("#productImage").click(function(){
+      $("#productImage").slideDown('1500').hide('1000');
+      $("#product").show('1500');
+    });
+    $("#product").click(function(){
+      $("#product").slideUp('1500');
+      $("#productImage").slideDown('1500');
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#work1").mouseover(function(){
+      $("#image1").show();
+    }).mouseout(function(){
+      $("#image1").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work2").mouseover(function(){
+      $("#image2").show();
+    }).mouseout(function(){
+      $("#image2").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work3").mouseover(function(){
+      $("#image3").show();
+    }).mouseout(function(){
+      $("#image3").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work4").mouseover(function(){
+      $("#image4").show();
+    }).mouseout(function(){
+      $("#image4").hide();
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#work5").mouseover(function(){
+      $("#image5").show();
+    }).mouseout(function(){
+      $("#image5").hide();
+    });
+    $("#work6").mouseover(function(){
+      $("#image6").show();
+    }).mouseout(function(){
+      $("#image6").hide();
+    });
+    $("#work7").mouseover(function(){
+      $("#image7").show();
+    }).mouseout(function(){
+      $("#image7").hide();
+    });
+    $("#work8").mouseover(function(){
+      $("#image8").show();
+    }).mouseout(function(){
+      $("#image8").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("form").submit(function(event){
+      event.preventDefault();
+      var name = $("input#name").val();
+      var email = $("input#email").val();
+      var message = $("textarea#comment").val();
+      if ($("input#name").val() && $("input#email").val()){
+        alert (name + ", we have received your message. Thank you for reaching out to us.");
+      }
+      else {
+        alert("Please enter your name and email!");
+      }
+      
+    });
+  
+  });*/
+  
+  
+  
