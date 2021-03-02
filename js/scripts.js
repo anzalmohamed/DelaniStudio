d709ea0f86ceae0b1d39eef08371cb51048ddf6a
anzalmohamed <anzaldidah@gmail.com>
2021-03-02 06:43:37 +0300

    added files

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-/*$(document).ready(function(){
+$(document).ready(function(){
     $("#developmentImage").click(function(){
       $("#developmentImage").slideDown('1500').hide('1000');
       $("#development").show('1500');
@@ -10,8 +10,8 @@
   });
   
   $(document).ready(function(){
-    $("#design-image").click(function(){
-      $("#design-image").slideDown('1500').hide('1000');
+    $("#designImage").click(function(){
+      $("#designImage").slideDown('1500').hide('1000');
       $("#design").show('1500');
     });
     $("#design").click(function(){
@@ -31,57 +31,6 @@
   });
   
   $(document).ready(function(){
-    $("#work1").mouseover(function(){
-      $("#image1").show();
-    }).mouseout(function(){
-      $("#image1").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#work2").mouseover(function(){
-      $("#image2").show();
-    }).mouseout(function(){
-      $("#image2").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#work3").mouseover(function(){
-      $("#image3").show();
-    }).mouseout(function(){
-      $("#image3").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#work4").mouseover(function(){
-      $("#image4").show();
-    }).mouseout(function(){
-      $("#image4").hide();
-    });
-  });
-  
-  $(document).ready(function(){
-    $("#work5").mouseover(function(){
-      $("#image5").show();
-    }).mouseout(function(){
-      $("#image5").hide();
-    });
-    $("#work6").mouseover(function(){
-      $("#image6").show();
-    }).mouseout(function(){
-      $("#image6").hide();
-    });
-    $("#work7").mouseover(function(){
-      $("#image7").show();
-    }).mouseout(function(){
-      $("#image7").hide();
-    });
-    $("#work8").mouseover(function(){
-      $("#image8").show();
-    }).mouseout(function(){
-      $("#image8").hide();
-    });
-  });
-  $(document).ready(function(){
     $("form").submit(function(event){
       event.preventDefault();
       var name = $("input#name").val();
@@ -96,7 +45,7 @@
       
     });
   
-  });*/
+  });
   
   
   
