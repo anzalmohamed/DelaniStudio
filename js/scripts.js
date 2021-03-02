c182e4d3ec24b99623adf48e4092f563081322ea
anzalmohamed <anzaldidah@gmail.com>
2021-03-02 14:19:52 +0300

    updated file

--- js/scripts.js
+++ js/scripts.js
@@ -49,74 +49,9 @@ $(document).ready(function(){
           $("#overlay").hide();
         });
       });
-      $(document).ready(function(){
-        $("#work2").mouseover(function(){
-          $("#overlay2").show();
-        }).mouseout(function(){
-          $("#overlay2").hide();
-        });
-      });
-      $(document).ready(function(){
-        $("#work3").mouseover(function(){
-          $("#overlay3").show();
-        }).mouseout(function(){
-          $("#overlay3").hide();
-        });
-      });
-      $(document).ready(function(){
-        $("#work4").mouseover(function(){
-          $("#overlay4").show();
-        }).mouseout(function(){
-          $("#overlay4").hide();
-        });
-      });
-      
      
-     /* $(document).ready(function(){$('.row').click(function(){
-        $('.work1').hover(function() {
-        $('#work1-overlay').show();
-      }).mouseout(function() {
-        $('#work1-overlay').hide();
-      });});
-      $(".work2").mouseover(function() {
-        $(".work2-overlay").show();
-      }).mouseout(function() {
-        $(".work2-overlay").hide();
-      });
-      $(".work3").mouseover(function() {
-        $(".work3-overlay").show();
-      }).mouseout(function() {
-        $(".work3-overlay").hide();
-      });
-      $(".work4").mouseover(function() {
-        $(".work4-overlay").show();
-      }).mouseout(function() {
-        $(".work4-overlay").hide();
-      });
-      $(".work5").mouseover(function() {
-        $(".work5-overlay").show();
-      }).mouseout(function() {
-        $(".work5-overlay").hide();
-      });
-      $(".work6").mouseover(function() {
-        $(".work6-overlay").show();
-      }).mouseout(function() {
-        $(".work6-overlay").hide();
-      });
-      $(".work7").mouseover(function() {
-        $(".work7-overlay").show();
-      }).mouseout(function() {
-        $(".work7-overlay").hide();
-      });
-      $(".work8").mouseover(function() {
-        $(".work8-overlay").show();
-      }).mouseout(function() {
-        $(".work8-overlay").hide();
-      });*/
-    
-      
     });
-  
+   
   });
   
   
